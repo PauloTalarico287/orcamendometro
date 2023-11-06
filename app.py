9177f4da4731ca8b488590654f1c9ac674bdca53
Paulo Talarico <66432564+PauloTalarico287@users.noreply.github.com>
2023-11-05 21:12:11 -0300

    Update app.py
    Tentando um novo jeito

--- app.py
+++ app.py
@@ -1,67 +1,54 @@
 import pandas as pd
 import requests
 import gspread
-import sendgrid
-import json
-import os
 from bs4 import BeautifulSoup
 from oauth2client.service_account import ServiceAccountCredentials
 from gspread_dataframe import get_as_dataframe, set_with_dataframe
 from flask import Flask, request
-from openpyxl import Workbook
-from openpyxl import load_workbook
-from sendgrid import SendGridAPIClient
-from sendgrid.helpers.mail import Mail, Email, To, Content
-from tchan import ChannelScraper
+import os  # Importar a biblioteca os para acessar variáveis de ambiente
 
+app = Flask(__name)
 
-SENDGRID_KEY = os.environ["SENDGRID_KEY"]
-GOOGLE_SHEETS_CREDENTIALS = os.environ["GOOGLE_SHEETS_CREDENTIALS"]
-with open("credenciais.json", mode="w") as fobj:
-  fobj.write(GOOGLE_SHEETS_CREDENTIALS)
-conta = ServiceAccountCredentials.from_json_keyfile_name("credenciais.json")
-api = gspread.authorize(conta)
+# Obter as chaves de ambiente diretamente
+SENDGRID_KEY = os.environ.get("SENDGRID_KEY")
+GOOGLE_SHEETS_CREDENTIALS = os.environ.get("GOOGLE_SHEETS_CREDENTIALS")
 
-app = Flask(__name__)
-
-if __name__ == '__main__':
-    app.run()
-
-@app.route("/")
-def index():
-  return "Quer saber mais do orçamento de SP?"
-
-@app.route("/subprefeituras")
-def subprefeituras():
-    global orcamento
-    orcamento = None
+def obter_dados_orcamento():
     url = "http://dados.prefeitura.sp.gov.br/dataset/7c34e3cc-e978-4810-a834-f8172c6ef81d/resource/cf3e5d80-8976-4d14-b139-4c820d6e9d35/download/basedadosexecucao0823.xlsx"
     response = requests.get(url)
     if response.status_code == 200:
         with open("basedadosexecucao0823.xlsx", "wb") as f:
             f.write(response.content)
-            df = pd.read_excel("basedadosexecucao0823.xlsx")
-            orcamento = df 
+        return pd.read_excel("basedadosexecucao0823.xlsx")
     else:
         print("Erro ao baixar o arquivo:", response.status_code)
+        return None
 
-    orc = orcamento[['Ds_Orgao', 'Ds_Programa', 'Ds_Projeto_Atividade', 'Vl_Orcado_Ano', 'Vl_Liquidado', 'Vl_Pago']]
-    Gastos = orc.groupby('Ds_Orgao')
-    investimento = Gastos.sum()
-    investimento = investimento.reset_index()
-    novos = ['Órgão', 'Valor orçado em 2023', 'Valor Liquidado', 'Valor Pago']
-    investimento.columns = novos
-    print(investimento)
-    investimento_por_sub = investimento[investimento['Órgão'].str.contains('Subprefeitura')]
-    pd.set_option('float_format', '{:.2f}'.format)
-    investimento_por_sub['Executado'] = investimento_por_sub['Valor Liquidado'] / investimento_por_sub['Valor orçado em 2023'] * 100
-    investimento_por_sub = investimento_por_sub.sort_values('Executado', ascending=False)
-    print(investimento_por_sub)
-    planilha = client.open_by_key("1Fwd76Zs_fyYWfJMhgROAHdvHLXYyt-uszcGtq5uHftk")
-    guia = planilha.worksheet("Subprefeituras")
-    data_to_append = investimento_por_sub.values.tolist()
-    data_to_append = [investimento_por_sub.columns.tolist()] + data_to_append
-    guia.clear()
-    for row in data_to_append:
-        guia.append_table(row)
+@app.route("/")
+def index():
+    return "Quer saber mais do orçamento de SP?"
+
+@app.route("/subprefeituras")
+def subprefeituras():
+    orcamento = obter_dados_orcamento()
+
+    if orcamento is not None:
+        orc = orcamento[['Ds_Orgao', 'Ds_Programa', 'Ds_Projeto_Atividade', 'Vl_Orcado_Ano', 'Vl_Liquidado', 'Vl_Pago']]
+        Gastos = orc.groupby('Ds_Orgao')
+        investimento = Gastos.sum().reset_index()
+        investimento.columns = ['Órgão', 'Valor orçado em 2023', 'Valor Liquidado', 'Valor Pago']
+
+        investimento_por_sub = investimento[investimento['Órgão'].str.contains('Subprefeitura')]
+        investimento_por_sub['Executado'] = investimento_por_sub['Valor Liquidado'] / investimento_por_sub['Valor orçado em 2023'] * 100
+        investimento_por_sub = investimento_por_sub.sort_values('Executado', ascending=False)
+
+        # Salvar os dados em uma planilha do Google Sheets
+        planilha = gspread.service_account(filename="credenciais.json")
+        guia = planilha.open_by_key("1Fwd76Zs_fyYWfJMhgROAHdvHLXYyt-uszcGtq5uHftk").worksheet("Subprefeituras")
+        guia.clear()
+        guia.insert_rows(investimento_por_sub.values.tolist(), 2)
+
     return "Novos dados atualizados"
+
+if __name__ == '__main__':
+    app.run()
