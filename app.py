cffcb04c941455952db69b7f8ab65f3884bb79d9
Paulo Talarico <66432564+PauloTalarico287@users.noreply.github.com>
2023-11-20 23:12:38 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 import gspread
-!pip install gspread oauth2client
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 from gspread_dataframe import get_as_dataframe, set_with_dataframe
